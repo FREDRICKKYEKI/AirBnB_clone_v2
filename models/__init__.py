f9af476f310d15f79eac3d9b125621b445a176d7
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-09-22 16:15:21 +0300

    commit before pull from remote

--- models/__init__.py
+++ models/__init__.py
@@ -8,6 +8,17 @@ from models.review import Review
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.place import Place
+from dotenv import load_dotenv
+
+load_dotenv()
+
+env_vars = ['HBNB_ENV','HBNB_MYSQL_USER','HBNB_MYSQL_PWD','HBNB_MYSQL_HOST',
+            'HBNB_MYSQL_DB',]
+
+for var in env_vars:
+    if not os.getenv(var):
+        raise Exception(f"Environment Varible {var} not found!")
+
 
 storage_type = os.getenv('HBNB_TYPE_STORAGE')
 storage = None
