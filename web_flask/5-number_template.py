11dadce193c0a0dfcbc0a5817bc33b2695654382
FREDRICK KYEKI <fredrickisaac142@gmail.com>
2023-09-22 14:36:28 +0300

    [fix]: assert pycodestyle compliance

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,7 +40,7 @@ def c_route(text):
     return f"C {text}"
 
 
-@app.route("/python/", defaults={"text" : "is cool"}, strict_slashes=False)
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_route(text):
     """python_route"""
