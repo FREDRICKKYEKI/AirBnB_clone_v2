d34e0b5cb7691b78201c69c281c6d5ca8076da3b
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-09-24 12:14:45 +0300

    [update]: add Airbnb clone features/templates

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,18 +3,24 @@
 Write a script that starts a Flask web application:
     - Your web application must be listening on 0.0.0.0, port 5000
     - You must use storage for fetching data from the storage engine
-      (FileStorage or DBStorage) => from models import storage and
-      storage.all(...)
+    (FileStorage or DBStorage) =>
+    from models import storage and storage.all(...)
+    - To load all cities of a State:
+        - If your storage engine is DBStorage, you must use cities
+        relationship
+        - Otherwise, use the public getter method cities
     - After each request you must remove the current SQLAlchemy Session:
         - Declare a method to handle @app.teardown_appcontext
         - Call in this method storage.close()
     - Routes:
-        - /states_list: display a HTML page: (inside the tag BODY)
-            - H1 tag: “States”
-            - UL tag: with the list of all State objects present in DBStorage
-              sorted by name (A->Z) tip
-                - LI tag: description of one State:
-                <state.id>: <B><state.name></B>
+        - /cities_by_states: display a HTML page: (inside the tag BODY)
+        - H1 tag: “States”
+        - UL tag: with the list of all State objects present in DBStorage
+          sorted by name (A->Z) tip
+            - LI tag: description of one State: <state.id>:
+              <B><state.name></B> + UL tag: with the list of City objects
+              linked to the State sorted by name (A->Z)
+            - LI tag: description of one City: <city.id>: <B><city.name></B>
     - Import this 7-dump to have some data
     - You must use the option strict_slashes=False in your route definition
 """
@@ -24,10 +30,10 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
     """Display a HTML page: (inside the tag BODY)"""
-    return render_template("7-states_list.html",
+    return render_template("8-cities_by_states.html",
                            states=storage.all(State).values())
 
 
---
+++ web_flask/9-states.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application:
+    - Your web application must be listening on 0.0.0.0, port 5000
+    - You must use storage for fetching data from the storage engine
+      (FileStorage or DBStorage) => from models import storage and
+      storage.all(...)
+    - After each request you must remove the current SQLAlchemy Session:
+        - Declare a method to handle @app.teardown_appcontext
+        - Call in this method storage.close()
+    - Routes:
+        - /states: display a HTML page: (inside the tag BODY)
+            - H1 tag: “States”
+            - UL tag: with the list of all State objects present in DBStorage
+              sorted by name (A->Z) tip
+                - LI tag: description of one State:
+                  <state.id>: <B><state.name></B>
+        - /states/<id>: display a HTML page: (inside the tag BODY)
+            - If a State object is found with this id:
+                - H1 tag: “State: ”
+                - H3 tag: “Cities:”
+                - UL tag: with the list of City objects linked to the State
+                  sorted by name (A->Z)
+                    - LI tag: description of one City: <city.id>:
+                      <B><city.name></B>
+            - Otherwise:
+            - H1 tag: “Not found!”
+    - You must use the option strict_slashes=False in your route definition
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/states', defaults={"id": None}, strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states(id):
+    """Display a HTML page: (inside the tag BODY)"""
+    if id is not None:
+        states = storage.all(State).values()
+        for state in states:
+            if state.id == id:
+                return render_template("9-states.html", state=state, city=True)
+        return render_template("9-states.html", city=False, not_found=True)
+    else:
+        return render_template("9-states.html", city=False,
+                               states=storage.all(State).values())
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
