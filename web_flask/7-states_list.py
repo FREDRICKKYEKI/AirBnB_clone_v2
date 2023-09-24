188732cd74b9d666ab85aea4364dad7767cb20dd
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-09-24 12:35:02 +0300

    [fix]: task7 (sort states)

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -21,14 +21,17 @@ Write a script that starts a Flask web application:
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+import os
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display a HTML page: (inside the tag BODY)"""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
     return render_template("7-states_list.html",
-                           states=storage.all(State).values())
+                           states=sorted_states)
 
 
 @app.teardown_appcontext
@@ -38,4 +41,4 @@ def tear_down(self):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)  # , debug=True)
